f2db3a4a735e9467e10e00f81f355f5b054ea45d
niphavadi <niphavadi@gmail.com>
2017-03-01 01:01:33 +0800

    Move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 try {
   var config = {
-      apiKey: "AIzaSyDyPE_vnLJ4mWxOQTcqj0i3TXGMz8KLX_Y",
-      authDomain: "reactproject1-786c2.firebaseapp.com",
-      databaseURL: "https://reactproject1-786c2.firebaseio.com",
-      storageBucket: "reactproject1-786c2.appspot.com",
-      messagingSenderId: "882758738059"
-    };
-    firebase.initializeApp(config);
-}catch(e){
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+  };
+
+  firebase.initializeApp(config);
+} catch (e) {
 
 }
 
