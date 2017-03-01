f8aa04aa5004659a50f3e62762d6dbc6f758dc52
niphavadi <niphavadi@gmail.com>
2017-03-01 10:21:46 +0800

    checkbox update

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -8,12 +8,12 @@ export var Todo = React.createClass({
     var {id, text, completed, createdAt, completedAt, dispatch} = this.props;
     var todoClassName = completed ? 'todo todo-completed' : 'todo';
     var renderDate = () => {
-      var message = 'Created ';
+      var message = 'Posted On ';
       var timestamp = createdAt;
 
       if (completed) {
-        message = 'Completed ';
-        timestamp = completedAt;
+        message = 'Not Available ';
+        //timestamp = completedAt;
       }
 
       return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
